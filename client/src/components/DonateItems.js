a05852eb27d99b0f75acfcfb92987dd25d21ead7
Lawrence Tucker <lawrence.ed.tucker@gmail.com>
2018-04-24 19:31:55 -0700

    Added React Router

---
+++ client/src/components/DonateItems.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Nav, NavItem } from 'react-bootstrap';
+import DonateRoutes from '../routes/DonateRoutes.js';
+
+export default class DonateItems extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Give spare items directly to people in need.</h2>
+                <h2>What could you spare?</h2>
+                <Nav bsStyle="pills" activeKey={1}>
+                    <NavItem eventKey={1} href="/clothing">Clothing</NavItem>
+                    <NavItem eventKey={2} href="/essentials">Essentials</NavItem>
+                    <NavItem eventKey={3} href="/hygiene">Hygiene</NavItem>
+                </Nav>
+                <DonateRoutes />
+            </div>
+        )
+    }
+}
