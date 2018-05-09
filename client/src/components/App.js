2d7e45eeca80c3a6305432ff758ecec6a3c41992
Vonetta <vonettastevenson@gmail.com>
2018-05-08 20:53:28 -0700

    added logo with a bootstrap navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,38 +1,52 @@
-import React, { Component } from 'react'
-import { Switch, Redirect, Route } from 'react-router-dom';
-import Request from './Request';
-import Donate from './Donate';
-import { Grid, PageHeader } from 'react-bootstrap';
-
+import React, { Component } from "react"
+import { Switch, Redirect, Route } from "react-router-dom"
+import Request from "./Request"
+import Donate from "./Donate"
+import { Grid, PageHeader, Navbar } from "react-bootstrap"
+import logo from "./../assets/logo/logo_spare.png"
 
 class App extends Component {
   render() {
     return (
-      <Grid>
+      <Grid fluid={true}>
         <PageHeader>
-          Spare
+          <div>
+            <Navbar fluid={true}>
+              <a href="/">
+                <img src={logo} alt="" />
+              </a>
+            </Navbar>
+          </div>
         </PageHeader>
-        <Switch>
-          <Route path='/donate' render={props => (<Donate mode='donate' {...props}/>)} />
-          <Redirect exact from="/" to="/donate" />
-          <Route path='/request' render={props => (<Request mode='request' {...props}/>)} />
-          <Route path='/code'>
-            <h2>Code</h2>
-          </Route>
-          <Route path='/about'>
-            <h2>About</h2>
-          </Route>
-          <Route path='/contact'>
-            <h2>Contact</h2>
-          </Route>
-          {/* TODO: 404 not working for nested routes */ }
-          <Route>
-            <h2>404 Not Found</h2>
-          </Route>
-        </Switch>
+        <div className="container">
+          <Switch>
+            <Route
+              path="/donate"
+              render={props => <Donate mode="donate" {...props} />}
+            />
+            <Redirect exact from="/" to="/donate" />
+            <Route
+              path="/request"
+              render={props => <Request mode="request" {...props} />}
+            />
+            <Route path="/code">
+              <h2>Code</h2>
+            </Route>
+            <Route path="/about">
+              <h2>About</h2>
+            </Route>
+            <Route path="/contact">
+              <h2>Contact</h2>
+            </Route>
+            {/* TODO: 404 not working for nested routes */}
+            <Route>
+              <h2>404 Not Found</h2>
+            </Route>
+          </Switch>
+        </div>
       </Grid>
-    );
+    )
   }
 }
 
-export default App;
+export default App
