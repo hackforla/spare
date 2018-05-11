2d12ba9f8d7d78d4fe5fba79db35fdb683f32d6b
Jordan Kusel <jordankusel@my.unt.edu>
2018-05-10 21:43:38 -0700

    renamed and moved assets, added Tile component for images

--- client/src/constants.js
+++ client/src/constants.js
@@ -28,56 +28,92 @@ export const itemTypesByCategory = {
 export const itemInfo = {
   shirts: {
     displayName: 'Shirts',
+    icon: 'shirts.png',
+    icon_lg: 'shirts@2x.png'
   },
   dresses_skirts: {
     displayName: 'Dresses + Skirts',
+    icon: 'dressesandskirts.png',
+    icon_lg: 'dressesandskirts@2x.png'
   },
   hats: {
     displayName: 'Hats',
+    icon: 'hats.png',
+    icon_lg: 'hats@2x.png'
   },
   pants: {
     displayName: 'Pants',
+    icon: 'pants.png',
+    icon_lg: 'pants@2x.png'
   },
   shorts: {
     displayName: 'Shorts',
+    icon: 'shorts.png',
+    icon_lg: 'shorts@2x.png'
   },
   jacket_sweaters: {
     displayName: 'Jackets + Sweaters',
+    icon: 'jacketsandsweaters.png',
+    icon_lg: 'jacketsandsweaters@2x.png'     
   },
   socks: {
     displayName: 'Socks',
+    icon: 'socks.png',
+    icon_lg: 'socks@2x.png'
   },
   shoes: {
     displayName: 'Shoes',
+    icon: 'shoes.png',
+    icon_lg: 'shoes@2x.png'
   },
   toothbrush: {
     displayName: 'Toothbrush',
+    icon: 'toothbrush.png',
+    icon_lg: 'toothbrush@2x.png'
   },
   toothpaste: {
     displayName: 'Toothpaste',
+    icon: 'toothpaste.png',
+    icon_lg: 'toothpaste@2x.png'
   },
   mouthwash: {
     displayName: 'Mouthwash',
+    icon: 'mouthwash.png',
+    icon_lg: 'mouthwash@2x.png'
   },
   soap: {
     displayName: 'Soap',
+    icon: 'soap.png',
+    icon_lg: 'soap@2x.png'
   },
   shampoo_conditioner: {
     displayName: 'Shampoo + Conditioner',
+    icon: 'shampoo.png',
+    icon_lg: 'shampoo@2x.png'
   },
   feminine_hygiene: {
     displayName: 'Feminine Hygiene',
+    icon: 'femininehygiene.png',
+    icon_lg: 'femininehygiene@2x.png'
   },
   blankets: {
     displayName: 'Blankets',
+    icon: 'blankets.png',
+    icon_lg: 'blankets@2x.png'
   },
   sleeping_bags: {
     displayName: 'Sleeping Bags',
+    icon: 'sleepingbags.png',
+    icon_lg: 'sleepingbags@2x.png'
   },
   cat_food: {
     displayName: 'Cat Food',
+    icon: 'catfood.png',
+    icon_lg: 'catfood@2x.png'
   },
   dog_food: {
     displayName: 'Dog Food',
+    icon: 'dogfood.png',
+    icon_lg: 'dogfood@2x.png'
   },
 }
