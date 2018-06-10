4b0a65395e863c316e1a2af2fd3177abe0c96fac
Jeff Koch <jeff@cuevaskoch.com>
2018-06-10 15:32:14 -0700

    Update global styling to better match prototype

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,47 +2,41 @@ import React, { Component } from "react"
 import { Switch, Redirect, Route } from "react-router-dom"
 import Request from "./Request"
 import Donate from "./Donate"
-import { Grid, PageHeader, Navbar } from "react-bootstrap"
-import logo from "./../assets/logo/logo_spare.png"
+import Header from './Header';
+import { Grid  } from "react-bootstrap"
 
 class App extends Component {
   render() {
     return (
       <Grid fluid={true}>
-        <PageHeader>
-          <div>
-            <Navbar fluid={true}>
-              <a href="/">
-                <img src={logo} alt="" />
-              </a>
-            </Navbar>
-          </div>
-        </PageHeader>
+        <Header />
         <div className="container">
-          <Switch>
-            <Route
-              path="/donate"
-              render={props => <Donate {...props} />}
-            />
-            <Redirect exact from="/" to="/donate" />
-            <Route
-              path="/request"
-              render={props => <Request {...props} />}
-            />
-            <Route path="/code">
-              <h2>Code</h2>
-            </Route>
-            <Route path="/about">
-              <h2>About</h2>
-            </Route>
-            <Route path="/contact">
-              <h2>Contact</h2>
-            </Route>
-            {/* TODO: 404 not working for nested routes */}
-            <Route>
-              <h2>404 Not Found</h2>
-            </Route>
-          </Switch>
+          <Grid>
+            <Switch>
+              <Route
+                path="/donate"
+                render={props => <Donate {...props} />}
+              />
+              <Redirect exact from="/" to="/donate" />
+              <Route
+                path="/request"
+                render={props => <Request {...props} />}
+              />
+              <Route path="/code">
+                <h2>Code</h2>
+              </Route>
+              <Route path="/about">
+                <h2>About</h2>
+              </Route>
+              <Route path="/contact">
+                <h2>Contact</h2>
+              </Route>
+              {/* TODO: 404 not working for nested routes */}
+              <Route>
+                <h2>404 Not Found</h2>
+              </Route>
+            </Switch>
+          </Grid>
         </div>
       </Grid>
     )
---
+++ client/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, PageHeader } from 'react-bootstrap';
+import logo from "./../assets/logo/logo_spare.png";
+import { Route, Switch } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+
+const renderFlipButton = (currentSide) => {
+  const isFromDonate = (currentSide === 'donate');
+  const next = isFromDonate ? 'request' : 'donate';
+  const className = `flip-button to-${next}`;
+  return () => (
+    <LinkContainer to={`/${next}`}>
+      <Button className={className}>{next} an item</Button>
+    </LinkContainer>
+  );
+}
+
+const header = () => (
+  <PageHeader className="header">
+    <LinkContainer to="/">
+      <img src={logo} alt="" />
+    </LinkContainer>
+    <Switch>
+        <Route
+          path="/donate"
+          render={renderFlipButton('donate')}
+        />
+        <Route
+          path="/request"
+          render={renderFlipButton('request')}
+        />
+    </Switch>
+  </PageHeader>
+);
+
+export default header;
