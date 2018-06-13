7b6d92280bcf306bfe4b2a024d22f05591c0c4a8
Jeffrey Koch <jeff@meinstein.org>
2018-06-12 20:53:42 -0700

    Load bootstrap from CDN

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-//bootstrap
-import './css/bootstrap.css';
-import './css/bootstrap-theme.css';
-
 //style sheets
 import './css/index.css';
 
