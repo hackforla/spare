ffea0e7bd18eb31aca6031de7cbc3560864e1c40
Shawn Her Many Horses <shawnhmh@gmail.com>
2018-09-02 10:35:36 -0700

    Clean up linting errors

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import { Button, PageHeader } from 'react-bootstrap';
-import { Route, Switch } from "react-router-dom";
+import { PageHeader } from 'react-bootstrap';
 import Logo from './Logo';
 import BreadcrumbNav from './BreadcrumbNav';
-import { LinkContainer } from 'react-router-bootstrap';
-
-const renderFlipButton = (currentSide) => {
-  const isFromDonate = (currentSide === 'donate');
-  const next = isFromDonate ? 'request' : 'donate';
-  const className = `flip-button to-${next}`;
-  return () => (
-    <span>
-      <LinkContainer to={`/${next}`}>
-        <Button className={className}>{next} an item</Button>
-      </LinkContainer>
-    </span>
-  );
-}
 
 const header = () => (
   <PageHeader className="header">
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -177,4 +177,4 @@ export const breakpoints = {
 
 export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const phoneRegex = /^\(\d{3}\) \d{3}\-\d{4}$/
+export const phoneRegex = /^\(\d{3}\) \d{3}-\d{4}$/
