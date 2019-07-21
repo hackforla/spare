3f20cee44a34ee479326158d06f2967d582d553e
Shawn Her Many Horses <shawnhmh@gmail.com>
2019-07-21 12:35:26 -0700

    Add column for donation request admin

--- server/donations/admin.py
+++ server/donations/admin.py
@@ -13,11 +13,29 @@ class DonationFulfillmentAdmin(admin.ModelAdmin):
         ('dropoff_date', DateRangeFilter),
     )
 
+
+class DropoffTimeAdmin(admin.ModelAdmin):
+    list_display = ('location', 'neighborhood', 'time_start', 'time_end')
+
+    def neighborhood(self, obj):
+        return obj.location.neighborhood
+
+    neighborhood.admin_order_field = 'location__neighborhood__name'
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request).prefetch_related(
+            'location', 'location__neighborhood')
+
+        return queryset
+
+    ordering = ('location__neighborhood__name', 'time_start')
+
+
 admin.site.register(Category)
 admin.site.register(DonationFulfillment, DonationFulfillmentAdmin)
 admin.site.register(DonationRequest)
 admin.site.register(Item)
 admin.site.register(Location)
 admin.site.register(Neighborhood)
-admin.site.register(DropoffTime)
+admin.site.register(DropoffTime, DropoffTimeAdmin)
 admin.site.register(ManualDropoffDate)
